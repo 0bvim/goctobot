0045b78a035631b2daf8b5de1f31051a0917b7aa
vinicius <niviciusdev@gmail.com>
2025-05-20 16:29:40 -0300

    Implement concurrent processing structure, remove follower methods

--- internal/api/client.go
+++ internal/api/client.go
@@ -1,8 +1,8 @@
 package api
 
 import (
-	"context"
 	"os"
+	"sync"
 
 	"github.com/google/go-github/v72/github"
 )
@@ -11,52 +11,15 @@ type GitHubClient struct {
 	client *github.Client
 }
 
+type ConcurrentProcessing struct {
+	wg       sync.WaitGroup
+	mu       sync.Mutex
+	allUsers []*github.User
+	errors   []error
+}
+
 func NewGitHubClient(token string) *GitHubClient {
 	return &GitHubClient{
 		client: github.NewClient(nil).WithAuthToken(os.Getenv("PERSONAL_GITHUB_TOKEN")),
 	}
-}
-
-func (c *GitHubClient) GetFollowers(ctx context.Context, username string) (followers []*github.User, err error) {
-	user, _, err := c.client.Users.Get(ctx, "")
-	if err != nil {
-		return nil, err
-	}
-
-	opt := &github.ListOptions{PerPage: 100}
-	for {
-		users, resp, err := c.client.Users.ListFollowers(ctx, *user.Login, opt)
-		if err != nil {
-			return nil, err
-		}
-		followers = append(followers, users...)
-		if resp.NextPage == 0 {
-			break
-		}
-		opt.Page = resp.NextPage
-	}
-
-	return followers, nil
-}
-
-func (c *GitHubClient) GetFollowing(ctx context.Context, username string) (following []*github.User, err error) {
-	user, _, err := c.client.Users.Get(ctx, "")
-	if err != nil {
-		return nil, err
-	}
-
-	opt := &github.ListOptions{PerPage: 100}
-	for {
-		users, resp, err := c.client.Users.ListFollowing(ctx, *user.Login, opt)
-		if err != nil {
-			return nil, err
-		}
-		following = append(following, users...)
-		if resp.NextPage == 0 {
-			break
-		}
-		opt.Page = resp.NextPage
-	}
-
-	return following, nil
 }
