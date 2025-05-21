e39929302e2398b833822775f13f7d15ca54a386
vinicius <niviciusdev@gmail.com>
2025-05-21 01:53:09 -0300

    chore: Simplify list command to use user stats instead of fetching lists

--- cmd/list.go
+++ cmd/list.go
@@ -7,10 +7,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sync"
 
 	"github.com/0bvim/goctobot/internal/api"
-	"github.com/google/go-github/v72/github"
 	"github.com/spf13/cobra"
 )
 
@@ -20,38 +18,39 @@ var listCmd = &cobra.Command{
 	Short: "List followers and following users",
 	Run: func(cmd *cobra.Command, args []string) {
 		client := api.NewGitHubClient(os.Getenv("GITHUB_PEROSNAL_TOKEN"))
-		var followers, following []*github.User
 		ctx := cmd.Context()
-		user, err := client.GetLogin(ctx)
+		user, err := client.GetUser(ctx)
 		if err != nil {
 			log.Fatalf("error getting user: %v", err)
 		}
 
-		var wg sync.WaitGroup
+		fmt.Printf("followers: %v\t\n", *user.Followers)
+		fmt.Printf("following: %v\t\n", *user.Following)
+		// NOTE: Use this to fetch followers and following concurrently in follow and unfollow commands
+		// var followers, following []*github.User
+		// var wg sync.WaitGroup
 
-		wg.Add(2)
-		go func() {
-			defer wg.Done()
-			var err error
-			followers, err = client.GetFollowers(ctx, *user)
-			if err != nil {
-				log.Println(err)
-			}
-		}()
+		// wg.Add(2)
+		// go func() {
+		// 	defer wg.Done()
+		// 	var err error
+		// 	followers, err = client.GetFollowers(ctx, *user)
+		// 	if err != nil {
+		// 		log.Println(err)
+		// 	}
+		// }()
 
-		go func() {
-			defer wg.Done()
-			var err error
-			following, err = client.GetFollowing(ctx, *user)
-			if err != nil {
-				log.Println(err)
-			}
-		}()
+		// go func() {
+		// 	defer wg.Done()
+		// 	var err error
+		// 	following, err = client.GetFollowing(ctx, *user)
+		// 	if err != nil {
+		// 		log.Println(err)
+		// 	}
+		// }()
 
-		wg.Wait()
+		// wg.Wait()
 
-		fmt.Printf("followers: %v\t\n", len(followers))
-		fmt.Printf("following: %v\t\n", len(following))
 	},
 }
 
